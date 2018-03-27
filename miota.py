e011279aa7c588be9978531d33ea672bb847e38c
nthon <nthon@live.com>
2018-03-27 07:33:35 +0700

    Update miota.py

--- miota.py
+++ miota.py
@@ -5,7 +5,6 @@
 # Feel free to used it in your own code
 # I this script I use PyOTA: https://github.com/iotaledger/iota.lib.py
 
-from iota import Iota
 
 def generate_seed():
     from random import SystemRandom
@@ -14,7 +13,38 @@ def generate_seed():
     generator = SystemRandom()
     return u''.join(generator.choice(alpha9) for _ in range(length))
 
+
+def encrypt_seed(seed, password):
+    # Remember that you must keep the return KEY to decrypt your ENCRYPTED SEED
+    import base64
+    import os
+    from cryptography.fernet import Fernet
+    from cryptography.hazmat.backends import default_backend
+    from cryptography.hazmat.primitives import hashes
+    from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+    salt = os.urandom(1024)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=1000000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    print(key)
+    f = Fernet(key)
+    # The output are encrypted seed and key
+    return [f.encrypt(seed), key]
+
+
+def decrypt_seed(encrypted_seed, key):
+    from cryptography.fernet import Fernet
+    f = Fernet(key)
+    return f.decrypt(encrypted_seed)
+
+
 def get_balance(seed, node):
+    from iota import Iota
     api = Iota(node, seed)
     acc_data = api.get_account_data()
     return acc_data['balance']
